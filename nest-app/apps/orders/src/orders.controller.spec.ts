f19bcb9f092bfcf5822aaa8f1596041ed0fc4260
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-14 20:14:50 -0300

    [:white_check_mark:]  | create: test - sevice (order)

--- nest-app/apps/orders/src/orders.controller.spec.ts
+++ nest-app/apps/orders/src/orders.controller.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 describe('OrdersController', () => {
   let ordersController: OrdersController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        PrismaModule,
+        ClientsModule.register([
+          {
+            name: 'ORDERS_SERVICE',
+            transport: Transport.KAFKA,
+            options: {
+              client: {
+                clientId: 'orders',
+                brokers: ['kafka:29092'],
+              },
+            },
+          },
+        ]),
+      ],
       controllers: [OrdersController],
       providers: [OrdersService],
     }).compile();
