7303acd936972d3973bd380979beb3b6f1b95887
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-14 19:04:19 -0300

    [:sparkles:]  | create: func - all (service/orders)

--- nest-app/apps/orders/src/orders.service.ts
+++ nest-app/apps/orders/src/orders.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma/prisma.service';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
 export class OrdersService {
+  constructor(
+    private prisma: PrismaService,
+    @Inject('ORDERS_SERVICE')
+    private kafkaClient: ClientKafka,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  all() {
+    return this.prisma.order.findMany();
   }
 }
