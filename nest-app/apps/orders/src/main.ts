9c70292413f906d5fecd290463cd044c83662f40
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-14 15:56:37 -0300

    [:gear:] | setup: swagger (docs)

--- nest-app/apps/orders/src/main.ts
+++ nest-app/apps/orders/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { OrdersModule } from './orders.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
+
+  const configDoc = new DocumentBuilder()
+    .setTitle('[Webstore Microsservices] - Orders')
+    .setDescription('Orders Microsservice that handle orders.')
+    .setVersion('1.0')
+    .addTag('Orders')
+    .setContact(
+      'Samuel Ricardo',
+      'https://www.linkedin.com/in/samuel-ricardo/',
+      'samuelricardoofficial@gmail.com',
+    )
+    .setLicense(
+      'MIT',
+      'https://github.com/Samuel-Ricardo/webstore_api/blob/main/LICENSE',
+    )
+    .build();
+
+  const docs = SwaggerModule.createDocument(app, configDoc);
+  SwaggerModule.setup('api/orders', app, docs);
+
   await app.listen(3000);
 }
 bootstrap();
