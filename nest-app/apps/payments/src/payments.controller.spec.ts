c54c7fde081ec2ef58ddad89709d2bbcba349ee1
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-14 23:19:26 -0300

    [:white_check_mark:]  | create: test - sevice (ppayments)

--- nest-app/apps/payments/src/payments.controller.spec.ts
+++ nest-app/apps/payments/src/payments.controller.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 describe('PaymentsController', () => {
   let paymentsController: PaymentsController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        PrismaModule,
+        ClientsModule.register([
+          {
+            name: 'PAYMENTS_SERVICE',
+            transport: Transport.KAFKA,
+            options: {
+              client: {
+                clientId: 'payments',
+                brokers: ['kafka:9092'],
+              },
+            },
+          },
+        ]),
+      ],
       controllers: [PaymentsController],
       providers: [PaymentsService],
     }).compile();
--- nest-app/apps/payments/src/payments.service.ts
+++ nest-app/apps/payments/src/payments.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma/prisma.service';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
 export class PaymentsService {
+  constructor(
+    private prisma: PrismaService,
+    @Inject('PAYMENTS_SERVICE')
+    private kafkaClient: ClientKafka,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  all() {
+    return this.prisma.payment.findMany();
   }
 }
