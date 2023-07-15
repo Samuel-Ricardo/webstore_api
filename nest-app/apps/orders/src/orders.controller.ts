3868987763167cf888516b231c37bec1ac04417f
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-14 22:15:26 -0300

    [:sparkles:]  | create: routes - all (controllers/orders)

--- nest-app/apps/orders/src/orders.controller.ts
+++ nest-app/apps/orders/src/orders.controller.ts
@@ -5,8 +5,13 @@ import { OrdersService } from './orders.service';
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
-  @Get()
+  @Get('/hello')
   getHello(): string {
     return this.ordersService.getHello();
+  }
+
+  @Get()
+  all() {
+    return this.ordersService.all();
   }
 }
