27aef94b34ddaf306683bb163118aaa184e218d6
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-14 22:20:45 -0300

    [:sparkles:]  | create: routes - create (controller/orders)

--- nest-app/apps/orders/src/orders.controller.ts
+++ nest-app/apps/orders/src/orders.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrderDTO } from './order.dto';
+import { OrderStatus } from '.prisma/client/orders';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class OrdersController {
@@ -19,5 +21,14 @@ export class OrdersController {
   @Post()
   async create(@Body() data: OrderDTO) {
     return this.ordersService.create(data);
+  }
+
+  @MessagePattern('payments')
+  async complete(@Payload() message: { order_id: number; status: string }) {
+    console.log(message);
+    await this.ordersService.complete(
+      message.order_id,
+      message.status === 'APROVED' ? OrderStatus.PAYED : OrderStatus.CANCELED,
+    );
   }
 }
